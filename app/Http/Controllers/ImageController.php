f5480c96df44479dc789802b2ea443199b00c46d
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2022-12-02 11:27:02 +0545

    rendering uploaded images done

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Image;
 
 class ImageController extends Controller
 {
-    //upload image
+    //
     public function uploadImage(Request $req)
     {
+        $req->validate([
+            "img" => "required",
+        ]);
         $image = $req->file('img');
-        $image->store('images', 'public');
+        $response = $image->store('images', 'public');
+        Image::create([
+            'path' => $response,
+        ]);
         return back();
+    }
+
+    public function renderImages()
+    {
+        $images = Image::all();
+        return view('welcome', compact('images'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,16 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="container border">
         <div class="row d-flex justify-content-center">
             <div class="col-md-7">
                 <div class="card mt-3">
                     <div class="card-header">
                         <h2>Image Upload</h2>
                     </div>
+
+                    {{-- <img src="{{asset('/storage/'.'images/7eYDuflPLrl410iVI0jJmzQ3I0xkAwNtcGGTj9wV.webp')}}" alt=""> --}}
+
                     <div class="card-body">
                         <form action="{{route('uploadImage')}}" method="post" class="form" enctype="multipart/form-data">
                             @csrf
@@ -28,6 +31,26 @@
                 </div>
             </div>
         </div>
+
+       
     </div>
+   
+    <div class="container  mt-2 border">
+        <h1 class="text-center mt-2">Photos</h1>
+        <div class="row justify-content-center">
+
+            @foreach($images as $image)
+            <div class="col-8 col-md-5 border m-2 p-0">
+                <div class="img-container" style="aspect-ratio:3/2;">
+                    
+                     <img src="{{asset('/storage/'.$image->path)}}" alt="" style="width:100%;height:100%;object-fit: cover;"> 
+                </div>
+            </div>
+
+            @endforeach
+         
+           
+        </div>
+    </div> 
 </body>
 </html>
